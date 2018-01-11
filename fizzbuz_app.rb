2947e4324c0d2fc700b7999b447bcfab4513ecc4
wvfeders <wvfedders@frontier.com>
2018-01-11 10:51:51 -0500

    some progress

--- fizzbuz_app.rb
+++ fizzbuz_app.rb
@@ -4,3 +4,15 @@ require_relative "fizzbuz_program.rb"
 get '/' do
 	erb :fizzbuz_home
 end
+
+post '/fizz_buzz_out' do
+	start = params[:start]
+	finish = param[:finish]
+	redirect 'start_finish?start=' + start + '&finish=' + finish
+end	
+
+get '/start_finish' do
+	start = params[:start]
+	finish = param[:finish]
+	erb :results, :locals => {:start => start, :finish => finish}
+end	
